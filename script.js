fb8a27acd8ac98bb5e1cd9fff39b1a9b20ceda86
dhanushrajkumar <dhanushrajkumar123@gmail.com>
2026-05-07 21:43:24 +0530

    SCRIPT A basic js script has been created

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Button click handler
+document.querySelector('.btn').addEventListener('click', () => {
+    document.querySelector('#projects').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+// Console message
+console.log('Welcome to my portfolio!');
